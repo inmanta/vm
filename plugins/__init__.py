a4d8380758fdd93804c014663368a938a4625421
Bart Vanbrabant <bart@vanbrabant.eu>
2015-11-17 16:16:01 +0100

    Do not use the id of the network

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -62,7 +62,7 @@ class Host(Resource):
     """
     fields = ("name", "flavor", "image", "key_name", "user_data", "key_value", "iaas_config", "purged", "network")
     map = {"key_name" : get_key_name, "key_value" : get_key_value, "iaas_config" : get_config,
-           "user_data" : get_user_data, "network": lambda _, vm: vm.network.id}
+           "user_data" : get_user_data, "network": lambda _, vm: vm.network.name}
 
 @plugin
 def names(host_prefix: "string", count: "number") -> "list":
